7aa784a7b0f689825fabf6d65c8a80cde25c0652
mukeshsakthivel <smukeshcse@gmail.com>
2024-08-21 21:59:44 +0530

    First commit

---
+++ EducationPost-Project/src/main/java/Socialmedia/EducationPost/Project/EducationPostProjectApplication.java
@@ -0,0 +1,13 @@
+package Socialmedia.EducationPost.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationPostProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationPostProjectApplication.class, args);
+	}
+
+}
---
+++ EducationPost-Project/src/test/java/Socialmedia/EducationPost/Project/EducationPostProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Socialmedia.EducationPost.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationPostProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
